1beac10df108d9d634cc7a9911d71cc19cc35ce0
caracio35 <caracio35@gmail.com>
2024-07-12 16:09:32 -0300

    cambio a mac el paht

--- tp_framework/src/main/java/nocito/Main.java
+++ tp_framework/src/main/java/nocito/Main.java
@@ -10,7 +10,7 @@ public class Main {
     public static void main(String[] args) {
 
         Framework framework = new Framework(
-                "/home/jose/Escritorio/tp_final_framework/tp_framework/src/main/resources/config.json");
+                "/Users/josehoracionocito/Documents/objeto/tp-final-framework/tp_framework/src/main/resources/config.json");
 
         // Simular selección de acciones desde el menú
         // Scanner scanner = new Scanner(System.in);
